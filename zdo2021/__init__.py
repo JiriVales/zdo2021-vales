04b5216ec73ffd704f07680bdd4a6d50fdf53ba2
JiriVales <38296763+JiriVales@users.noreply.github.com>
2021-06-08 20:31:37 +0200

    Update __init__.py

--- zdo2021/__init__.py
+++ zdo2021/__init__.py
@@ -1,9 +1,3 @@
- adresar = "mytest"
- startobr = 1
- pocetobr = 10
- vyska = 0 #nepracoval jsem s tim v algoritmu, predpokladam datasety s podobnymi vlastnosti (rozliseni, vzdalenost, velikost obr, barevne kanaly)
- sirka = 0 #nepracoval jsem s tim v algoritmu, predpokladam datasety s podobnymi vlastnosti (rozliseni, vzdalenost, velikost obr, barevne kanaly)
- barevne_kanaly = 3 #nepracoval jsem s tim v algoritmu, predpokladam datasety s podobnymi vlastnosti (rozliseni, vzdalenost, velikost obr, barevne kanaly)
- data = [pocetobr,vyska,sirka,barevne_kanaly]
- result = VarroaDetector.predict(startobr, adresar, data)
+ data = "mytest/1.jpg"
+ result = VarroaDetector.predict(null, data)
  print(result)
